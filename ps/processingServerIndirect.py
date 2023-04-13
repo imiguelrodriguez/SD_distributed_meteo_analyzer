06eba19a856d8e50dabf219a275b3a871d9e67c1
Ignacio Miguel Rodríguez <ignacio.miguel@estudiants.urv.cat>
2023-04-13 13:02:15 +0200

    Now it allows to exist several workers being fed from the same queue.

--- ps/processingServerIndirect.py
+++ ps/processingServerIndirect.py
@@ -2,17 +2,22 @@ import os
 import pika
 import sys
 
+import time
+
+
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='pollution_data')
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        print(" [x] Received %r" % body.decode())
+        time.sleep(4)
+        print(" [x] Done")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='pollution_data', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
